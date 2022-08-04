6dae497a1154dbdab5c296bbe51808169e797740
MichalTheBear <misiekfaken@gmail.com>
2022-08-04 15:58:54 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+let currentNum = "";
+let nextNum = "";
+
+let buttons = document.querySelectorAll(".buttons");
+let resultDisplay = document.querySelector(".resultDisplay");
+
+let plus = document.querySelector(".plus");
+let equal = document.querySelector(".equal");
+
+//first number
+
+for (let button of buttons) {
+    button.addEventListener("click", (e) => {
+        currentNum = parseInt(currentNum + e.target.textContent);
+        console.log(currentNum);
+        resultDisplay.innerHTML = currentNum;
+    })
+}
+
+
+
+
+// first number and then click PLUS button and enter second number
+
+plus.addEventListener("click", () => {
+    for (let button of buttons) {
+        button.addEventListener("click", (e) => {
+            nextNum = parseInt(nextNum + e.target.textContent);
+            console.log(nextNum);
+            resultDisplay.innerHTML = nextNum;
+        })
+    }
+    equal.addEventListener("click", () => {
+        resultDisplay.innerHTML = `${currentNum} and ${nextNum}`;
+    })
+})
